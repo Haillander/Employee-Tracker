c670581a375756439b5651320f4480fc5291e045
Haillander <haillanderviana@gmail.com>
2023-02-13 16:20:32 +1100

    Added employees names, role and department

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,18 @@
 INSERT INTO departments (department_name)
-VALUES ("IT"),
+VALUES ("Engineering"),
        ("Finance"),
-       ("Legal"),
-       ("Marketing");
+       ("Markenting"),
+       ("Legal");
 
 INSERT INTO roles (job_title, department_id, salary)
-VALUES ("Marketing Manager", 4, 135030),
-       ("Accountant", 2, 73560),
-       ("Software Engineer", 1, 94011),
-       ("Lawyer", 3, 126930),
-       ("Front End Developer", 1, 79177);
+VALUES ("Marketing Manager", 4, 58000),
+       ("Acount Manager", 2, 73560),
+       ("Software Engineer", 1, 25890),
+       ("Lawyer", 3, 1005820),
+       ("back End Developer", 1, 95802);
 
 INSERT INTO employees (first_name, last_name, role_id, department_id, manager_id)
-VALUES ("Bradley", "Jester", 1, 4, NULL),
-       ("Mackenzie", "Olson", 2, 2, 1),
-       ("Rosalinda", "Ramirez", 3, 1, 1),
-       ("Haneia", "Simpson", 4, 3, 1);
+VALUES ("Jhon", "Doe", 1, 4, NULL),
+       ("Mike", "chan", 2, 2, 1),
+       ("Sarah", "Rebecca", 3, 1, 1),
+       ("Kevin", "Milgarde", 4, 3, 1);
